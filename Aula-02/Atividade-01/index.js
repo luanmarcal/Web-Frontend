1e7c8652920e8f5171120c2a122d39a1c33f6ae6
luanmarcal <luan.marcal@padolabs.com.br>
2022-06-29 17:40:38 -0300

    :rofl: adding exercise02

---
+++ Aula-02/Atividade-01/index.js
@@ -0,0 +1,76 @@
+const buttonEl = document.querySelector(".e101_22")
+const secEl = document.querySelector(".e101_18")
+const minEl = document.querySelector(".e101_13")
+const hourEl = document.querySelector(".e101_8")
+const textEl = document.querySelector(".e101_25")
+let select;
+
+let sec = 0;
+let min = 0;
+let hour = 0;
+let flag = 0;
+
+
+function adicionaSegundo() {
+    sec += 1;
+
+    if (sec >= 59) {
+        sec = 0;
+        min += 1;
+    }
+
+    if (min >= 59) {
+        min = 0;
+        hour += 1;
+    }
+}
+
+buttonEl.onclick = () => {
+    flag += 1
+
+    if (flag == 1) {
+        textEl.textContent = "Stop";
+        buttonEl.classList.remove("color02")
+        buttonEl.classList.remove("e101_22")
+        buttonEl.classList.add("color")
+
+        select = setInterval(() => {
+            adicionaSegundo();
+            secEl.textContent = sec;
+            minEl.textContent = min;
+            hourEl.textContent = hour;
+
+        }, 10)
+    }
+
+    if (flag == 2) {
+        textEl.textContent = "Reset";
+        buttonEl.classList.remove("e101_22")
+        buttonEl.classList.remove("color")
+        buttonEl.classList.add("color02")
+
+        clearInterval(select)
+    }
+
+    if (flag == 3) {
+        sec = 0;
+        min = 0;
+        hour = 0;
+        flag2 = 0;
+        flag = 0;
+
+        textEl.textContent = "Start";
+        buttonEl.classList.remove("color")
+        buttonEl.classList.remove("color02")
+        buttonEl.classList.add("e101_22")
+
+        secEl.textContent = sec;
+        minEl.textContent = min;
+        hourEl.textContent = hour;
+    }
+
+}
+
+
+
+
