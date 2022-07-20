c9a98776df18052fe4692a555202e4c93a881c02
luanmarcal <luan.marcal@padolabs.com.br>
2022-07-20 17:37:14 -0300

    :books new functions

--- Aula-03/Atividade-01/index.js
+++ Aula-03/Atividade-01/index.js
@@ -1,9 +1,95 @@
-const tarefaEl = document.querySelector('#tarefa')
+const tarefasEl = document.querySelector('.tarefas')
+//const tarefaEl = document.querySelector('.tarefa')
 const botaoAddEl = document.querySelector('#botaoAdd')
-const barraPesquisaEl = document.querySelector('#barraPesquisa').value
+const barraPesquisaEl = document.querySelector('#barraPesquisa')
+// const botaoDelEl = document.querySelectorAll('.trashButton')
 
-botaoAddEl.onclick = console.log(document.querySelector('#barraPesquisa').value)
+const numberPendentesEl = document.querySelector('.numberPendentes')
+const numberCompletasEl = document.querySelector('.numberCompletas')
 
+let text = "";
+let completas = 0;
+let pendentes = 0;
+
+botaoAddEl.onclick = () => {
+    console.log(barraPesquisaEl.value)
+    text = barraPesquisaEl.value;
+    apiCreateTask(text, false)
+    MountTask(); 
+}
+
+
+async function MountTask() {
+    let task = await apiGetTask();
+    console.log(task);
+
+    tarefasEl.innerText = "";
+
+    task.forEach((task) => {
+        let newTask = document.createElement('div')
+        newTask.classList.add('tarefa')
+
+        let taskDiv = document.createElement('div')
+        newTask.appendChild(taskDiv);
+
+        let check = document.createElement('input');
+        check.type = 'checkbox';
+
+        if (task.complete === true) {
+            check.setAttribute('checked', task.complete);
+            completas++;
+        }
+        else
+            pendentes++;
+
+        taskDiv.appendChild(check);
+
+        let nameTask = document.createElement('label');
+        nameTask.innerText = " " + task.description;
+        taskDiv.appendChild(nameTask);
+
+        let buttonTrash = document.createElement('button');
+        buttonTrash.classList.add('trashButton')
+
+
+        let iconTrash = document.createElement('img');
+        iconTrash.src = "./trash.svg"
+        iconTrash.setAttribute("taskId", task.id)
+
+        buttonTrash.onclick = (event) => {
+            console.log("del")
+            console.log(event.target)
+            const element = event.target
+            const taskid = element.getAttribute("taskId")
+            apiDeleteTasks(taskid);
+            let taskElement = element.parentElement.parentElement
+            tarefasEl.removeChild(taskElement)
+            let check = element.parentElement.parentElement.firstChild.firstChild
+            console.log(check)
+            if(check.getAttribute(checked))
+                completas--;
+            else
+                pendentes--;
+
+
+            console.log(check)
+  
+            numberPendentesEl.innerText = pendentes;
+        }
+        
+        buttonTrash.appendChild(iconTrash);
+
+        newTask.appendChild(buttonTrash);
+
+        tarefasEl.appendChild(newTask);
+    })
+
+    numberCompletasEl.innerText = completas;
+    numberPendentesEl.innerText = pendentes;
+
+    return console.log("foi o teste")
+
+}
 
 async function apiCreateTask(description, complete) {
     const url = 'http://localhost:3000/tasks'
@@ -26,7 +112,7 @@ async function apiGetTask() {
 
     const response = await fetch(url)
 
-    return console.log(await response.json())
+    return await response.json()
 }
 
 
@@ -74,9 +160,8 @@ async function apiDeleteTasks(id) {
 }
 // apiCreateTask("task", false)
 
-apiGetTask()
+MountTask();
 
 // apiUpdateTask(1, false)
 
 // apiDeleteTasks(1);
-
